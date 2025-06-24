86fbb9daf6a8d56a40c7a8aaf85a740ede6bce17
Макаров Степан Александрович <st.mkv03@gmail.com>
2025-06-24 11:41:34 +0300

    feat(global coroutines)

--- src/curses_tools/__init__.py
+++ src/curses_tools/__init__.py
@@ -3,7 +3,11 @@ all = (
     "get_frame_size",
     "read_controls",
     "get_frames",
+    "sleep",
+    "update_speed",
 )
 
 from .frame import draw_frame, get_frame_size, get_frames
 from .key_control import read_controls
+from .animation import sleep
+from .physics import update_speed
--- src/main.py
+++ src/main.py
@@ -5,9 +5,18 @@ import os.path
 import random
 import time
 
-from animation import fire
-from curses_tools import draw_frame, get_frame_size, read_controls, get_frames
+from animation import fire, fly_garbage
+from curses_tools import (
+    draw_frame,
+    get_frame_size,
+    read_controls,
+    get_frames,
+    sleep,
+    update_speed,
+)
 
+SPACESHIP_FRAME = ""
+COROUTINES = []
 STAR_SYMBOL = "+*.:"
 STARS_COUNT = 100
 STARS_ROW, STARS_COLUMN = 5, 2
@@ -24,28 +33,30 @@ async def blink(
     offset_tics: int,
     symbol: str = "*",
 ) -> None:
-    for _ in range(offset_tics):
-        await asyncio.sleep(0)
+    await sleep(offset_tics)
 
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        for _ in range(20):
-            await asyncio.sleep(0)
+        await sleep(20)
 
         canvas.addstr(row, column, symbol)
-        for _ in range(3):
-            await asyncio.sleep(0)
+        await sleep(3)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        for _ in range(5):
-            await asyncio.sleep(0)
+        await sleep(5)
 
         canvas.addstr(row, column, symbol)
-        for _ in range(3):
-            await asyncio.sleep(0)
+        await sleep(3)
+
+async def animate_spaceship(rocket_frames):
+    global SPACESHIP_FRAME
+
+    for frame in itertools.cycle(rocket_frames):
+        SPACESHIP_FRAME = frame
+        await sleep(2)
 
 
-async def animate_spaceship(
+async def control_spaceship(
     canvas: "curses.window",
     row: int,
     column: int,
@@ -54,12 +65,17 @@ async def animate_spaceship(
     prev_height, prev_width = row, column
     min_x, min_y = 1, 1
     max_x, max_y = canvas.getmaxyx()
-    end_frame = None
+    global SPACESHIP_FRAME
+    end_frame = SPACESHIP_FRAME
     frame_cycle = itertools.cycle(frames)
+    weight_speed = 0
+    height_speed = 0
 
     for frame in frame_cycle:
         delta_row, delta_column, space = read_controls(canvas)
         frame_rows, frame_columns = get_frame_size(frame)
+
+        weight_speed, height_speed = update_speed(weight_speed, height_speed, delta_row, delta_column)
 
         if (
             delta_column + frame_columns + prev_width > max_y - 1
@@ -84,32 +100,49 @@ async def animate_spaceship(
         await asyncio.sleep(0)
 
 
+async def fill_orbit_with_garbage(canvas, garbage_frames):
+    max_width = canvas.getmaxyx()[1]
+
+    while True:
+        COROUTINES.append(
+            fly_garbage(
+                canvas, random.randint(1, max_width), random.choice(garbage_frames)
+            )
+        )
+        await sleep(10)
+
+
 def draw(canvas: "curses.window") -> None:
     curses.curs_set(False)
     canvas.nodelay(True)
     canvas.border()
 
     max_height, max_width = canvas.getmaxyx()
-    coroutines = []
     for _ in range(STARS_COUNT):
         row = random.randint(1, max_height - 2)
         col = random.randint(1, max_width - 2)
         symbol = random.choice(STAR_SYMBOL)
-        coroutines.append(
+        COROUTINES.append(
             blink(canvas, row, col, random.randint(0, STARS_COUNT), symbol)
         )
 
-    coroutines.append(fire(canvas, max_height // 2, max_width // 2))
+    COROUTINES.append(fire(canvas, max_height // 2, max_width // 2))
 
-    frames = get_frames(STARSHIP_FRAMES_DIR, repeat=2)
-    coroutines.append(animate_spaceship(canvas, max_height // 2, max_width // 2, frames))
+    starship_frames = get_frames(STARSHIP_FRAMES_DIR, repeat=2)
+    garbage_frames = get_frames(GARBAGE_FRAMES_DIR)
+
+    COROUTINES.append(
+        control_spaceship(canvas, max_height // 2, max_width // 2, starship_frames)
+    )
+    COROUTINES.append(fill_orbit_with_garbage(canvas, garbage_frames))
+    COROUTINES.append(animate_spaceship(starship_frames))
 
     while True:
-        for corutine in coroutines.copy():
+        for corutine in COROUTINES.copy():
             try:
                 corutine.send(None)
             except StopIteration:
-                coroutines.remove(corutine)
+                COROUTINES.remove(corutine)
             canvas.refresh()
         time.sleep(TIC_TIMEOUT)
 
